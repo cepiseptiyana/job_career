ecce0b67512e5ef0551453f03103601bf3bb7742
Cepi Septiyana <cepifams3@gmail.com>
2026-08-29 10:24:49 +0700

    use database local

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Job;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
--- database/migrations/2025_09_08_154401_create_jobs_table.php
+++ database/migrations/2025_09_08_154401_create_jobs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->foreignId('category_id')->constrained('categories', 'id')->onDelete('cascade');
             $table->foreignId('job_type_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users', 'id')->onDelete('cascade');
+            // $table->foreignId('user_id')->constrained('users', 'id')->onDelete('cascade');
             $table->integer('vacancy');
             $table->string('salary')->nullable();
             $table->string('location');
